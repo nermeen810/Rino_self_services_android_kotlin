d4f5e3a896f7d90e351874b918294afd5a86fa92
AndoridDEV <nermeen.abdel.azeem810@gmail.com>
2022-06-01 07:57:49 -0700

    payment processes home design and implementation

---
+++ app/src/main/java/com/rino/self_services/ui/paymentProcessHome/SubPaymentItemAdapter.kt
@@ -0,0 +1,67 @@
+package com.rino.self_services.ui.paymentProcessHome
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.rino.self_services.R
+import com.rino.self_services.databinding.PaymentHistoryItemBinding
+import com.rino.self_services.model.pojo.payment.Items
+import com.rino.self_services.utils.Constants
+
+
+class SubPaymentItemAdapter (private var paymentSubList: ArrayList<Items>,
+                             private val paymentHomeViewModel: PaymentHomeViewModel, private val context: Context
+) : RecyclerView.Adapter<SubPaymentItemAdapter.SubPaymentItemViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+
+    ): SubPaymentItemViewHolder {
+        return SubPaymentItemViewHolder(
+            PaymentHistoryItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return paymentSubList.size
+    }
+
+    override fun onBindViewHolder(holder: SubPaymentItemViewHolder, position: Int) {
+        holder.binding.serviceNumValue.text = Constants.convertNumsToArabic(paymentSubList[position].id.toString())
+        val dateRes = paymentSubList[position].date.split("T")
+        holder.binding.dateFromTxt.text     = Constants.convertNumsToArabic(dateRes[0])
+        holder.binding.amoouuntValu.text     = Constants.convertNumsToArabic(paymentSubList[position].amount.toString())
+        holder.binding.agencyValue.text     = Constants.convertNumsToArabic(paymentSubList[position].department?:"")
+        holder.binding.paymentMethodValue.text     = context.getText(R.string.cash)
+        holder.binding.requestStatusValue.text     = paymentSubList[position].status?:""
+        holder.binding.requestToValue.text  = paymentSubList[position].current?.users?.get(0)
+
+        //      holder.binding.timeTxt.text         = historyList[position].createdDate?: "00/00/00 00:00".split(" ").toList()[1]
+        holder.binding.card.setOnClickListener {
+  //          paymentHomeViewModel.navToServiceDetails(historyList[position])
+        }
+
+    }
+
+
+    fun updateItems(newList: List<Items>) {
+        paymentSubList.clear()
+        paymentSubList.addAll(newList)
+        notifyDataSetChanged()
+    }
+    fun clearList() {
+        paymentSubList.clear()
+        notifyDataSetChanged()
+    }
+    inner class SubPaymentItemViewHolder(val binding: PaymentHistoryItemBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+}
+
+
