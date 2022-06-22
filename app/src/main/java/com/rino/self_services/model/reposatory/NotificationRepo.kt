3bb61cebde8420b38c9a7406dadc2ba042d45ba8
AndoridDEV <nermeen.abdel.azeem810@gmail.com>
2022-06-22 10:47:59 -0700

    handle is login  and refresh token

--- app/src/main/java/com/rino/self_services/model/reposatory/NotificationRepo.kt
+++ app/src/main/java/com/rino/self_services/model/reposatory/NotificationRepo.kt
@@ -7,9 +7,11 @@ import com.rino.self_services.model.dataSource.localDataSource.MySharedPreferenc
 import com.rino.self_services.model.dataSource.localDataSource.Preference
 import com.rino.self_services.model.dataSource.localDataSource.PreferenceDataSource
 import com.rino.self_services.model.dataSource.remoteDataSource.ApiDataSource
+import com.rino.self_services.model.pojo.login.RefreshTokenResponse
 import com.rino.self_services.model.pojo.notifications.AllNotificationResponse
 import com.rino.self_services.model.pojo.notifications.NotificationCountResponse
 import com.rino.self_services.model.pojo.notifications.SetNotificationAsRead
+import com.rino.self_services.utils.Constants
 import com.rino.self_services.utils.PREF_FILE_NAME
 import com.rino.self_services.utils.Result
 import java.io.IOException
@@ -24,6 +26,54 @@ class NotificationRepo @Inject constructor(private val apiDataSource: ApiDataSou
 
     private val sharedPreference: Preference = PreferenceDataSource(preference)
 
+    private suspend fun refreshToken(): Result<RefreshTokenResponse?> {
+        var result: Result<RefreshTokenResponse?> = Result.Loading
+
+        try {
+            val response = apiDataSource.refreshToken("refresh_token",sharedPreference.getRefreshToken(),
+                Constants.client_id)
+            if (response.isSuccessful) {
+                result = Result.Success(response.body())
+                Log.i("refreshToken", "Result $result")
+                sharedPreference.setToken(response.body()?.accessToken!!)
+                sharedPreference.setRefreshToken(response.body()?.refreshToken!!)
+            } else {
+                Log.i("refreshToken", "Error${response.message()}")
+                when (response.code()) {
+                    400 -> {
+                        Log.e("Error 400", "Bad Request")
+                        result = Result.Error(Exception("حدث حطأ برجاء اعادة تسجيل الدخول"))
+                        sharedPreference.logout()
+                        Log.i("refreshToken refresh token:", "Result $result")
+
+                    }
+                    404 -> {
+                        Log.e("Error 404", "Not Found")
+                    }
+
+                    500 -> {
+                        Log.e("Error 500", "Server Error")
+                        result = Result.Error(Exception("server is down"))
+                    }
+                    502 -> {
+                        Log.e("Error 502", "Time out")
+                        result =
+                            Result.Error(Exception("حدث خطأ أثناء الاتصال بالانترنت برجاء فحص الشبكة"))
+                    }
+                    else -> {
+                        Log.e("Error", "Generic Error")
+                    }
+                }
+            }
+
+        } catch (e: IOException) {
+            result = Result.Error(e)
+            Log.e("ModelRepository", "IOException ${e.message}")
+            Log.e("ModelRepository", "IOException ${e.localizedMessage}")
+
+        }
+        return result
+    }
     suspend fun getNotificationCount(): Result<NotificationCountResponse?> {
         var result: Result<NotificationCountResponse?> = Result.Loading
         try {
@@ -41,6 +91,28 @@ class NotificationRepo @Inject constructor(private val apiDataSource: ApiDataSou
                         Log.e("Error 404", "Not Found")
                         //  result = Result.Error(Exception("Not Found"))
                     }
+                    401 ->{
+                        Log.e("Error 401", "Not Auth please, logout and login again")
+                        if (sharedPreference.isLogin()) {
+                            Log.i(
+                                "Model Repo:",
+                                "isLogin:" + sharedPreference.isLogin() + ", token:" + sharedPreference.getToken() + ",  refresh token:" + sharedPreference.getRefreshToken()
+                            )
+                            val res = refreshToken()
+                            when(res) {
+                                is Result.Success -> {
+                                    result = Result.Error(Exception("حدث حطأ برجاء اعادة المحاولة "))
+                                }
+                                is Result.Error -> {
+                                    result = Result.Error(Exception("حدث حطأ برجاء تسجيل الخروج ثم اعادة تسجيل الدخول"))
+                                }
+                            }
+                        }
+                        else {
+                            result =
+                                Result.Error(Exception("حدث حطأ برجاء تسجيل الخروج ثم اعادة تسجيل الدخول"))
+                        }
+                    }
                     500 -> {
                         Log.e("Error 500", "Server Error")
                         result = Result.Error(Exception("server is down"))
@@ -48,7 +120,7 @@ class NotificationRepo @Inject constructor(private val apiDataSource: ApiDataSou
                     502 -> {
                         Log.e("Error 502", "Time out")
                         result =
-                            Result.Error(Exception("time out"))
+                            Result.Error(Exception("حدث حطأ برجاء اعادة المحاولة "))
                     }
                     else -> {
                         Log.e("Error", "Generic Error")
@@ -83,6 +155,28 @@ class NotificationRepo @Inject constructor(private val apiDataSource: ApiDataSou
                         Log.e("Error 404", "Not Found")
                         //  result = Result.Error(Exception("Not Found"))
                     }
+                    401 ->{
+                        Log.e("Error 401", "Not Auth please, logout and login again")
+                        if (sharedPreference.isLogin()) {
+                            Log.i(
+                                "Model Repo:",
+                                "isLogin:" + sharedPreference.isLogin() + ", token:" + sharedPreference.getToken() + ",  refresh token:" + sharedPreference.getRefreshToken()
+                            )
+                            val res = refreshToken()
+                            when(res) {
+                                is Result.Success -> {
+                                    result = Result.Error(Exception("حدث حطأ برجاء اعادة المحاولة "))
+                                }
+                                is Result.Error -> {
+                                    result = Result.Error(Exception("حدث حطأ برجاء تسجيل الخروج ثم اعادة تسجيل الدخول"))
+                                }
+                            }
+                        }
+                        else {
+                            result =
+                                Result.Error(Exception("حدث حطأ برجاء تسجيل الخروج ثم اعادة تسجيل الدخول"))
+                        }
+                    }
                     500 -> {
                         Log.e("Error 500", "Server Error")
                         result = Result.Error(Exception("server is down"))
@@ -90,7 +184,7 @@ class NotificationRepo @Inject constructor(private val apiDataSource: ApiDataSou
                     502 -> {
                         Log.e("Error 502", "Time out")
                         result =
-                            Result.Error(Exception("time out"))
+                            Result.Error(Exception("حدث حطأ برجاء اعادة المحاولة "))
                     }
                     else -> {
                         Log.e("Error", "Generic Error")
@@ -125,6 +219,28 @@ class NotificationRepo @Inject constructor(private val apiDataSource: ApiDataSou
                         Log.e("Error 404", "Not Found")
                         //  result = Result.Error(Exception("Not Found"))
                     }
+                    401 ->{
+                        Log.e("Error 401", "Not Auth please, logout and login again")
+                        if (sharedPreference.isLogin()) {
+                            Log.i(
+                                "Model Repo:",
+                                "isLogin:" + sharedPreference.isLogin() + ", token:" + sharedPreference.getToken() + ",  refresh token:" + sharedPreference.getRefreshToken()
+                            )
+                            val res = refreshToken()
+                            when(res) {
+                                is Result.Success -> {
+                                    result = Result.Error(Exception("حدث حطأ برجاء اعادة المحاولة "))
+                                }
+                                is Result.Error -> {
+                                    result = Result.Error(Exception("حدث حطأ برجاء تسجيل الخروج ثم اعادة تسجيل الدخول"))
+                                }
+                            }
+                        }
+                        else {
+                            result =
+                                Result.Error(Exception("حدث حطأ برجاء تسجيل الخروج ثم اعادة تسجيل الدخول"))
+                        }
+                    }
                     500 -> {
                         Log.e("Error 500", "Server Error")
                         result = Result.Error(Exception("server is down"))
@@ -132,7 +248,7 @@ class NotificationRepo @Inject constructor(private val apiDataSource: ApiDataSou
                     502 -> {
                         Log.e("Error 502", "Time out")
                         result =
-                            Result.Error(Exception("time out"))
+                            Result.Error(Exception("حدث حطأ برجاء اعادة المحاولة "))
                     }
                     else -> {
                         Log.e("Error", "Generic Error")
